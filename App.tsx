6552b66a0a06818ffe7d69c9354dd7a2d13b9d80
Huynh Duc Dung <dunghd.it@gmail.com>
2023-12-26 23:14:59 +0800

    feat: add react native animated

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
 import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
-	return <HomeScreen />;
+	return (
+		<SafeAreaProvider>
+			<HomeScreen />
+		</SafeAreaProvider>
+	);
 }
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+import AnimatedStyle from "../assets/components/AnimatedStyle";
 
 export default function HomeScreen() {
+	const insets = useSafeAreaInsets();
+
 	return (
-		<View style={styles.container}>
+		<View style={{ ...styles.container, paddingTop: insets.top }}>
 			<Text style={{ fontFamily: "CommitMono", fontSize: 30 }}>
 				Home Screen
 			</Text>
+			<AnimatedStyle />
 			<StatusBar style="auto" />
 		</View>
 	);
