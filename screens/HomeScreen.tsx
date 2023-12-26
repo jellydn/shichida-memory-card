86ff7b82bdd4585d7a006ff3945434edd2b2962c
Huynh Duc Dung <dunghd.it@gmail.com>
2023-12-26 12:34:26 +0800

    feat: add embed font with expo v50 preview 7

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+	return (
+		<View style={styles.container}>
+			<Text style={{ fontFamily: "CommitMono", fontSize: 30 }}>
+				Home Screen
+			</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
